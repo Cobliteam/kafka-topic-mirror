536facdb8703a2a7459aa478aeda259a01d3cf3f
Ivan Stoiev <ivan.stoiev@cobli.co>
2020-07-21 00:11:06 -0300

    implement topic diff between two kafkas

--- src/main/scala/co/cobli/kafka/mirror/TopicsMirrorCommand.scala
+++ src/main/scala/co/cobli/kafka/mirror/TopicsMirrorCommand.scala
@@ -1,59 +1,97 @@
 
 package co.cobli.kafka.mirror
 
-import java.util.Properties
+import joptsimple.{ArgumentAcceptingOptionSpec, OptionParser, OptionSet, OptionSpecBuilder}
+import kafka.utils.{CommandLineUtils, Exit, Logging}
+import org.apache.kafka.common.utils.Utils
 
-import scala.collection.JavaConverters._
-import scala.collection._
-
-import joptsimple.{ArgumentAcceptingOptionSpec, OptionParser, OptionSet, OptionSpec, OptionSpecBuilder}
-import kafka.utils.{CommandLineUtils, Logging}
-
-
-object TopicsMirrorCommand extends Logging {
+object TopicsMirrorCommand extends Logging with TopicsComparator {
 
   def main(args: Array[String]): Unit = {
 
-    val opts = new TopicsMetadataUpsertCommandOptions(args)
+    val opts = new TopicsMirrorCommandOptions(args)
 
-    if (args.length == 0 || opts.options.hasArgument(opts.helpOpt))
-      CommandLineUtils.printUsageAndDie(opts.parser, "Mirror topics between Kafkas.")
+    opts.checkArgs(args)
 
-    opts.checkArgs()
+    var exitCode = 0
 
-  }
-
-  class TopicsMetadataUpsertCommandOptions(args: Array[String]) {
-    val parser = new OptionParser(false)
-    val zkConnectOptSrc: ArgumentAcceptingOptionSpec[String] = parser.accepts("zookeeper-src", "REQUIRED: The connection string for the " +
-      "source zookeeper connection in the form host:port. Multiple hosts can be given to allow fail-over.")
-      .withRequiredArg
-      .describedAs("hosts")
-      .ofType(classOf[String])
-    val kafkaConnectOptDst: ArgumentAcceptingOptionSpec[String] = parser.accepts("bootstrap-servers-dst", "REQUIRED: The connection string " +
-      "for the destination kafka connection in the form host:port.")
-      .withRequiredArg
-      .describedAs("hosts")
-      .ofType(classOf[String])
-
-    val commandConfigPropertyOptDst: ArgumentAcceptingOptionSpec[String] = parser.accepts("command-config-property-dst", "A mechanism to " +
-      "pass user-defined properties in the form key=value to the destination kafka Admin Client connection. Multiple entries allowed.")
-      .withRequiredArg
-      .describedAs("dst_prop")
-      .ofType(classOf[String])
-
-    val dryRunOpt: OptionSpecBuilder = parser.accepts("dry-run", "Just list topics differences without mirroring anything.")
-    val helpOpt: OptionSpecBuilder = parser.accepts("help", "Print usage information.")
-
-    val options: OptionSet = parser.parse(args: _*)
-
-    val configPropsDst: Properties = CommandLineUtils.parseKeyValueArgs(options.valuesOf(commandConfigPropertyOptDst).asScala)
-
-    def checkArgs() {
-      // check required args
-      CommandLineUtils.checkRequiredArgs(parser, options, zkConnectOptSrc)
-      CommandLineUtils.checkRequiredArgs(parser, options, kafkaConnectOptDst)
+    def onException(e: Throwable): Unit ={
+      println("Error while executing topic command : " + e.getMessage)
+      error(Utils.stackTrace(e))
+      exitCode = 1
     }
+
+    def withKafkaClient(bootstrapServers: String)(f: TopicsReaderClient => Unit ): Unit = {
+      try {
+        val kafkaClient = new TopicsReaderClient(bootstrapServers)
+        f(kafkaClient)
+      } catch {
+        case e: Throwable=> onException(e)
+      }
+    }
+
+    withKafkaClient(opts.sourceBootstrapServers) {sourceClient => {
+      withKafkaClient(opts.destinationBootstrapServers) { destinationClient => {
+        try {
+          val diffs = compareTopics(sourceClient.getTopicsInfo(), destinationClient.getTopicsInfo())
+          for ((topic, diff) <- diffs) {
+            println(s"$topic: ${diff.toString}")
+          }
+        } catch {
+          case e: Throwable => onException(e)
+        } finally {
+          sourceClient.close()
+          destinationClient.close()
+          Exit.exit(exitCode)
+        }
+      }}
+      sourceClient.close()
+      Exit.exit(exitCode)
+    }}
+    Exit.exit(exitCode)
   }
 
+  class TopicsMirrorCommandOptions(args: Array[String]) {
+    private val parser = new OptionParser(false)
+
+    private val bootstrapServersSrc: ArgumentAcceptingOptionSpec[String] = parser.accepts("bootstrap-servers-src", "REQUIRED: The " +
+      "connection string for the source kafka connection in the form host:port.")
+      .withRequiredArg
+      .describedAs("hosts")
+      .ofType(classOf[String])
+
+    private val bootstrapServersDst: ArgumentAcceptingOptionSpec[String] = parser.accepts("bootstrap-servers-dst", "REQUIRED: The " +
+      "connection string for the destination kafka connection in the form host:port.")
+      .withRequiredArg
+      .describedAs("hosts")
+      .ofType(classOf[String])
+
+    private val dryRunOpt: OptionSpecBuilder = parser.accepts("dry-run", "Just list topics differences without mirroring anything.")
+    private val helpOpt: OptionSpecBuilder = parser.accepts("help", "Print usage information.")
+
+    private val options: OptionSet = {
+      try {
+        parser.parse(args: _*)
+      } catch {
+        case e: Throwable => CommandLineUtils.printUsageAndDie(parser, e.getMessage)
+      }
+    }
+
+    def checkArgs(args: Array[String]) {
+      // check required args
+      if (args.length == 0 || isHelp)
+        CommandLineUtils.printUsageAndDie(parser, "Mirror topics between Kafkas.")
+
+      CommandLineUtils.checkRequiredArgs(parser, options, bootstrapServersSrc, bootstrapServersDst)
+    }
+
+    def isHelp: Boolean = options.has(helpOpt)
+
+    def isDryRun: Boolean = options.has(dryRunOpt)
+
+    def sourceBootstrapServers: String = options.valueOf(bootstrapServersSrc)
+
+    def destinationBootstrapServers: String = options.valueOf(bootstrapServersDst)
+
+  }
 }
